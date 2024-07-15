55fe48c2c11d6cda2103feba6312acfaae531d04
ulicaki8991 <157748097+ulicaki8991@users.noreply.github.com>
2024-07-15 15:19:51 +0200

    Bird.cs

--- Bird.cs
+++ Bird.cs
@@ -8,6 +8,11 @@ public class Bird : MonoBehaviour
     public float jumpForce = 5f; // Adjust this value to control jump height
 
     private Rigidbody rb;
+    public GM gm;
+
+    [Header("StartGame")]
+    [SerializeField] float LimitY;
+    
 
     void Start()
     {
@@ -20,10 +25,21 @@ public class Bird : MonoBehaviour
         {
             Jump();
         }
+
+
+        if(!gm.GameStarted && gameObject.transform.position.y <= LimitY)
+        {
+            Jump();
+        }
     }
 
     void Jump()
     {
         rb.velocity = Vector3.up * jumpForce; // Apply upward force to jump
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gm.GameEnd();
     }
 }
